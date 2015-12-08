49388dd5849a23a3dea49aa835a9b451ea25ec2f
Jacek Markowski <jacek87markowski@gmail.com>
2015-12-08 23:25:43 +0100

    <readline/history.h>

--- src/lisp.h
+++ src/lisp.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <readline/readline.h>
 #ifdef LISP_SEATEST
 #include "../src/mpc/mpc.h"
 #else
 #include "mpc/mpc.h"
 #endif
-
+#include <readline/readline.h>
+#include <readline/history.h>
 double evalOp(double x, char *op, double y);
 double eval(mpc_ast_t* t);
 char* lispParseEvaluate(char* expr);
